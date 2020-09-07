092d61b32f20d372dc56930bbb9650bfa907fba0
DontTouchMyMind <tobigface@gmail.com>
2020-09-07 15:57:35 +0200

    Rebased functions.

--- multithreading/01_problem_demo.py
+++ multithreading/01_problem_demo.py
@@ -1,27 +1,4 @@
-def read_ints(path):
-    lst = []
-    with open(path, 'r') as f:
-        while line := f.readline():
-            lst.append(int(line))
-
-    return lst
-
-
-def count_three_sum(ints):
-    print('Started count_three_sum function...')
-
-    n = len(ints)
-    counter = 0
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
-                if ints[i] + ints[j] + ints[k] == 0:
-                    counter += 1
-                    print(f'Triple found: {ints[i]}, {ints[j]}, {ints[k]}')
-
-    print(f'Ended count_three_sum. Triplets counter={counter}')
-
+from multithreading.count_three_sum import count_three_sum, read_ints
 
 if __name__ == '__main__':
     print('Started main.')
