40c50472db3c81c037e1bdcfe1b2d1f2a7365dd8
DontTouchMyMind <tobigface@gmail.com>
2020-10-05 11:10:20 +0200

    Added new class.

--- multithreading/14_04_cancel_uow.py
+++ multithreading/14_04_cancel_uow.py
@@ -4,22 +4,32 @@ import time
 from multithreading.count_three_sum import read_ints
 
 
-class ThreeSumUnitOfWork(threading.Thread):
+class StoppableThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self.stop_event.set()
+
+    def stopped(self):
+        return self.stop_event.is_set()
+
+
+class ThreeSumUnitOfWork(StoppableThread):
 
     def __init__(self, ints, name='TestThread'):
         super().__init__(name=name)
         self.ints = ints
-        self.stop_event = threading.Event()
-
-
+        # self.stop_event = threading.Event()
 
     def run(self):
         print(f'{self.getName()} starts')
         self.count_three_sum(self.ints)
         print(f'{self.getName()} ends')
 
-    def cancel(self):
-        self.stop_event.set()
+    # def cancel(self):
+    #     self.stop_event.set()
 
     def count_three_sum(self, ints):
         print(f'Started count_three_sum')
@@ -30,7 +40,7 @@ class ThreeSumUnitOfWork(threading.Thread):
         for i in range(n):
             for j in range(i + 1, n):
                 for k in range(j + 1, n):
-                    if self.stop_event.is_set():
+                    if super().stopped():
                         print('Task was cancelled')
                         counter = -1
                         return counter
@@ -51,7 +61,7 @@ if __name__ == '__main__':
     task.start()
     time.sleep(5)
 
-    task.cancel()
+    task.stop()
     task.join()
 
     print('Ended Main')
