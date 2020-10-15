2cabf332fd7ab8cd1c820366c1145dd2486b97a1
DontTouchMyMind <tobigface@gmail.com>
2020-10-15 12:16:49 +0200

    Added file after viewing lesson.

---
+++ asyncio/02_async_aiohttp.py
@@ -0,0 +1,45 @@
+import asyncio
+
+import aiohttp
+
+from multithreading.decorators import async_measure_time
+
+
+class Photo:
+    def __init__(self, album_id, photo_id, title, url, thumbnail_url):
+        self.url = url
+        self.thumbnail_url = thumbnail_url
+        self.title = title
+        self.photo_id = photo_id
+        self.album_id = album_id
+
+    # Добавим статический метод, который парсит объект json и возвращает объект фотографии.
+    @classmethod
+    def from_json(cls, obj):
+        return Photo(obj['albumId'], obj['id'], obj['title'], obj['url'], obj['thumbnailUrl'])
+
+
+def print_photo_title(photos):
+    for photo in photos:
+        print(f'{photo.title}', end='\n')
+
+
+async def photos_by_album(task_name, album, session):
+    print(f'{task_name=}')
+    url = f'https://jsonplaceholder.typicode.com/photos?albumId={album}'
+
+    response = await session.get(url)
+    photos_json = await response.json()
+
+    return [Photo.from_json(photo) for photo in photos_json]
+
+
+@async_measure_time
+async def main():
+    async with aiohttp.ClientSession() as session:
+        photos = await photos_by_album('Task 1', 3, session)
+        print_photo_title(photos)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
